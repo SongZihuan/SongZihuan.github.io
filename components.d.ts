f62083aa344ad981f01db937940df17cc92a9907
宋子桓🌈 <songzihuan@song-zh.com>
2025-01-28 23:01:45 +0800

    增加了github page提示
    Signed-off-by: 宋子桓🌈 <songzihuan@song-zh.com>

--- components.d.ts
+++ components.d.ts
@@ -13,6 +13,7 @@ declare module '@vue/runtime-core' {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElButtonGroup: typeof import('element-plus/es')['ElButtonGroup']
     ElCard: typeof import('element-plus/es')['ElCard']
+    ElDialog: typeof import('element-plus/es')['ElDialog']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElImage: typeof import('element-plus/es')['ElImage']
--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,49 @@
 
   document.addEventListener('DOMContentLoaded', setIcon)
 
+  const hostname = window.location.hostname
+  const githubDialogVisible = ref(false)
+
+  if (hostname === 'songzihuan.github.io') {
+    githubDialogVisible.value = true
+  }
+
+  const onClose = () => {
+    githubDialogVisible.value = false
+  }
+
+  const toWebsite1 = () => {
+    window.location.href = 'https://song-zh.com'
+  }
+
+  const toWebsite2 = () => {
+    window.location.href = 'https://www.song-zh.com'
+  }
+
   placeholderLoading()
 </script>
 
 <template>
   <router-view></router-view>
+  <el-dialog v-model="githubDialogVisible" title="警告">
+    <div>
+      您当前访问的站点是由 Github Page 所托管。但本项目为 Vue3 的单页项目（SPA）。Github Page
+      无法提供很好的支持，因此建议你访问主站。
+      <br />
+      主站地址1：https://song-zh.com
+      <br />
+      主站地址2：https://www.song-zh.com
+    </div>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button-group>
+          <el-button type="success" size="large" @click="toWebsite1">前往主站地址1</el-button>
+          <el-button type="primary" size="large" @click="toWebsite2">前往主站地址2</el-button>
+          <el-button type="warning" size="large" @click="onClose">关闭</el-button>
+        </el-button-group>
+      </div>
+    </template>
+  </el-dialog>
 </template>
 
 <style>
